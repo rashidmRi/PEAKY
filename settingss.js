1aef4f406481cbefec506c18b1040265479a3292
rashidmRi <rashidmari302@gmail.com>
2025-11-02 21:08:20 +0500

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "blinder~5ZBniDrA#Abf_4HAZ-fkFmVb699ffGHwm9LETvaXknX--L9M8wgE",  // add your Session Id make sure it starts with blinder~
+  SESSION_ID: "blinder~JQhHDLTT#pTG36Mw8DoGPqUcInTL0YC7iiwTuMr0Q-xTijvs5woQ",  // add your Session Id make sure it starts with blinder~
   OWNER_NUMBER: "254116266407", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
